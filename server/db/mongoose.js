3398abc22134faa69498f50cfc787c70de67e93e
techspiderman <adm@MacRetina13s-MacBook-Pro.local>
2018-04-04 12:36:33 -0700

    setup heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;//need to tell mongoose which promise library to use
-mongoose.connect(process.eng.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
